7a5f120115e5fb1b35d727610a672d08cd745898
dpldevil <yong9xi@gmail.com>
2019-10-20 20:19:26 +0700

    Add Get/Set

--- app/src/main/java/com/example/notes/Model.java
+++ app/src/main/java/com/example/notes/Model.java
@@ -7,4 +7,36 @@ public class Model {
     private String notes;
     private Date time;
     private String tag;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
 }
