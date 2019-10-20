a627bdeeac3118c55af670b2297b16b6ff2ed44b
dpldevil <yong9xi@gmail.com>
2019-10-20 20:17:19 +0700

    Add class Model

---
+++ app/src/main/java/com/example/notes/Model.java
@@ -0,0 +1,10 @@
+package com.example.notes;
+
+import java.util.Date;
+
+public class Model {
+    private String title;
+    private String notes;
+    private Date time;
+    private String tag;
+}
