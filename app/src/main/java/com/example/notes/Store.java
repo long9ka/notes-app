d8cc6afb2dde1fb070409c9a543c7580d81cffb5
dpldevil <yong9xi@gmail.com>
2019-10-20 23:04:17 +0700

    Custom Shared Preferences

---
+++ app/src/main/java/com/example/notes/Store.java
@@ -0,0 +1,35 @@
+package com.example.notes;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class Store extends AppCompatActivity {
+    private SharedPreferences sharedPreferences;
+    private List<Model> modelList;
+    private Gson gson;
+
+    Store(Context context) {
+        gson = new Gson();
+        sharedPreferences = context.getSharedPreferences("data", MODE_PRIVATE);
+    }
+
+    public void getStore() {
+        String json = sharedPreferences.getString("list-items", null);
+        Type type = new TypeToken<List<Model>>(){}.getType();
+        modelList = gson.fromJson(json, type);
+    }
+
+    public void setStore() {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        String json = gson.toJson(modelList);
+        editor.putString("list-items", json).apply();
+    }
+}
