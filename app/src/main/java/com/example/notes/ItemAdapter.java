272d4bc3fde323df7eab3d96b3a6fd02b6807e93
dpldevil <yong9xi@gmail.com>
2019-10-22 22:32:30 +0700

    custom Filter

--- app/src/main/java/com/example/notes/ItemAdapter.java
+++ app/src/main/java/com/example/notes/ItemAdapter.java
@@ -5,18 +5,34 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class ItemAdapter extends ArrayAdapter<Model> {
     private Context context;
     private int resource;
     private List<Model> objects;
+    private List<Model> getObjects;
+    private List<Integer> realPos;
+
+    public List<Integer> getRealPos() {
+        return realPos;
+    }
+
+    private void initRealPos() {
+        realPos = new ArrayList<>();
+        for(int i = 0; i < getObjects.size(); i++) {
+            realPos.add(i);
+        }
+    }
 
     private class ViewHolder {
         TextView title;
@@ -29,6 +45,8 @@ public class ItemAdapter extends ArrayAdapter<Model> {
         this.context = context;
         this.resource = resource;
         this.objects = objects;
+        this.getObjects = new ArrayList<>(objects);
+        initRealPos();
     }
 
     @NonNull
@@ -50,4 +68,40 @@ public class ItemAdapter extends ArrayAdapter<Model> {
 
         return convertView;
     }
+
+    @NonNull
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults filterResults = new FilterResults();
+                if (constraint == null || constraint.length() == 0) {
+                    initRealPos();
+                    filterResults.values = getObjects;
+                } else {
+                    realPos.clear();
+                    List<Model> list = new ArrayList<>();
+                    String string = constraint.toString().toLowerCase().trim();
+                    for (int i = 0; i < getObjects.size(); i++) {
+                        if (getObjects.get(i).getTitle().toLowerCase().contains(string)|| getObjects.get(i).getTag().toLowerCase().contains(string)) {
+                            list.add(getObjects.get(i));
+                            realPos.add(i);
+                        }
+                    }
+                    filterResults.values = list;
+                }
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                objects.clear();
+                objects.addAll((Collection<? extends Model>) results.values);
+                notifyDataSetChanged();
+            }
+        };
+
+    }
+
 }
--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.notes;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import androidx.appcompat.widget.SearchView;
 
@@ -37,6 +41,36 @@ public class MainActivity extends AppCompatActivity {
         adapter = new ItemAdapter(this, R.layout.list_item, store.modelList);
         listView = findViewById(R.id.list_item);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                openNewActivity(adapter.getRealPos().get(position));
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle(store.modelList.get(position).getTitle())
+                        .setMessage("Do you want to delete this note?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                store.getStore();
+                                store.modelList.remove(store.modelList.get(adapter.getRealPos().get(position).intValue()));
+                                adapter.notifyDataSetChanged();
+                                store.setStore();
+                                recreate();
+                            }
+                        })
+                        .setNegativeButton("No", null)
+                        .show();
+                return true;
+            }
+        });
+
         super.onResume();
     }
 
